dc785229730494ac0f2c0b8b15a2ce4316d1cdff
Temi <ytemiloluwa@gmail.com>
2022-11-10 14:58:26 +0100

    [ Chore : Added GenreViewModel ]

--- PodcastApp/ViewModels/GenreViewModel.swift
+++ PodcastApp/ViewModels/GenreViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct GenreViewModel: Identifiable, Hashable {
+    
+    var id: Int
+    var name: String
+    static let placeHolder = createPlaceholder(id: 1)
+    
+    static var listPlaceHolders: [GenreViewModel] {
+        
+        (0..<15).map {
+            
+            createPlaceholder(id: $0)
+        }
+    }
+    
+    private static func createPlaceholder(id: Int) -> GenreViewModel {
+        
+        GenreViewModel(Genre(id: id, name: "Technology"))
+        
+    }
+    
+    init(_ genre: Genre) {
+        
+        self.id = genre.id
+        self.name = genre.name
+    }
+
+}
