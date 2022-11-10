c72610e66ab5fad5f0069790c00977fd0c55580d
Temi <ytemiloluwa@gmail.com>
2022-11-10 13:52:20 +0100

    [ Chore : Podcast & Genre model ]

---
+++ PodcastApp/Models/Genre.swift
@@ -0,0 +1,19 @@
+//
+//  Genre.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 10/11/2022.
+//
+
+import Foundation
+
+struct GenreList: Codable {
+    
+    let genre: [Genre]
+}
+
+struct Genre: Codable {
+    
+    let id: Int
+    let name: String
+}
---
+++ PodcastApp/Models/Podcast.swift
@@ -0,0 +1,43 @@
+//
+//  Podcast.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 10/11/2022.
+//
+
+import Foundation
+
+struct PodcastList: Codable {
+    
+    var resultCount: Int
+    let result: [Podcast]
+}
+
+struct Podcast: Codable {
+    
+    var artistID: Int?
+    var collectionID, trackID: Int
+    var description: String?
+    var artistName, collectionName, trackName: String?
+    var feedURL: String?
+    var artworkUrl30, artworkUrl60, artworkUrl100: String?
+    var releaseDate: String?
+    var collectionExplicitness, trackExplicitness: String?
+    var trackCount: Int?
+    var country, primaryGenreName: String?
+    var contentAdvisoryRating: String?
+    var artworkUrl600: String?
+    var genres: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+
+        case artistID = "artistId"
+        case collectionID = "collectionId"
+        case trackID = "trackId"
+        case artistName, collectionName, trackName
+        case feedURL = "feedUrl"
+        case artworkUrl30, artworkUrl60, artworkUrl100
+        case releaseDate, collectionExplicitness, trackExplicitness, trackCount, country, primaryGenreName, contentAdvisoryRating, artworkUrl600
+        case genres
+    }
+}
