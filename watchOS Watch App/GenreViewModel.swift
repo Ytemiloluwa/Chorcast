d0d6d5bc9bf7bb2b5595a2941bea8ad236fe357f
Temi <ytemiloluwa@gmail.com>
2022-11-10 14:31:47 +0100

    [ Chore : Added GenreViewModel to ViewModel ]

---
+++ watchOS Watch App/GenreViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  GenreViewModel.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 10/11/2022.
+//
+
+import Foundation
+
+struct GenreViewModel: Identifiable, Hashable {
+    
+    var id: Int
+    var name: String
+    
+    static let placeHolder = createPlaceholder(id: 1)
+    
+    static var listPlaceHolders: [GenreViewModel] {
+        
+        (0..<15).map {
+            
+            createPlaceholder(id: $0)
+        }
+    }
+    
+    private static func createPlaceholder(id: Int) -> GenreViewModel {
+        
+        GenreViewModel(Genre(id: id, name: "Technology"))
+        
+    }
+    
+    init(_ genre: Genre) {
+        
+        self.id = genre.id
+        self.name = genre.name
+    }
+
+}
+
