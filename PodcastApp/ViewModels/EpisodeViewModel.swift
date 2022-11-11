5235488d9392eecf41d86dac2a3e8da5120f2498
Temi <ytemiloluwa@gmail.com>
2022-11-11 15:53:35 +0100

    [ Chore: Created Placeholder for Redacted Views ]

--- PodcastApp/ViewModels/EpisodeViewModel.swift
+++ PodcastApp/ViewModels/EpisodeViewModel.swift
@@ -17,8 +17,25 @@ struct EpisodeViewModel: Identifiable {
     var audio: Data?
     var audioLength: Double
     var audioSize: String
-    var podcast: GenreViewModel = .placeHolder
+   // var podcast: GenreViewModel = .placeHolder
     
+    static var placeholder: EpisodeViewModel {
+        
+        createPlaceholder()
+    }
+    
+    static var listPlaceholder: [EpisodeViewModel] {
+        
+        (0...10).map { _ in
+        
+            createPlaceholder()
+        }
+    }
+    
+    private static func createPlaceholder() -> EpisodeViewModel {
+        
+        EpisodeViewModel(tittle: "Lorem ipsum", description: "", date:  "20 sept 2020", audioUrl: "", audioLength: 120, audioSize: "13MB")
+    }
     
 }
 
