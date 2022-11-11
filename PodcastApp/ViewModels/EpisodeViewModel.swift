43cdbc710933de129cbcdbcfdcaf83c44bf32f11
Temi <ytemiloluwa@gmail.com>
2022-11-11 15:09:14 +0100

    [ Chore : Declared Properties of the view ]

--- PodcastApp/ViewModels/EpisodeViewModel.swift
+++ PodcastApp/ViewModels/EpisodeViewModel.swift
@@ -8,4 +8,17 @@
 import Foundation
 import FeedKit
 
+struct EpisodeViewModel: Identifiable {
+    
+    let id = UUID()
+    var tittle, description: String
+    var date: String
+    var audioUrl: String
+    var audio: Data?
+    var audioLength: Double
+    var audioSize: String
+    var podcast: GenreViewModel = .placeHolder
+    
+    
+}
 
