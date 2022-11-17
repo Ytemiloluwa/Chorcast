69e318edec2536313102ce056cc8f033fc74d24b
Temi <ytemiloluwa@gmail.com>
2022-11-17 09:53:17 +0100

    [ Chore : Network Observation ]

--- PodcastApp/Networking/InternetMonitoring.swift
+++ PodcastApp/Networking/InternetMonitoring.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Network
+
+class InternetMonitor: ObservableObject {
+    
+    private let monitor: NWPathMonitor = NWPathMonitor()
+    
+    @Published var isAvailable = true
+    
+    init() {
+        
+        let queue = DispatchQueue(label: "Temi.PodcastApp.InternetMonitor")
+        monitor.pathUpdateHandler = { path in
+            
+            DispatchQueue.main.async {
+                
+                self.isAvailable = path.status == .satisfied
+            
+            }
+        }
+    }
+}
