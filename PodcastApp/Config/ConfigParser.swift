b834d51cf4cd581842c14d921b14beb256b6cc2a
Temi <ytemiloluwa@gmail.com>
2022-11-18 00:00:48 +0100

    [ Chore : ConfigParser ]

---
+++ PodcastApp/Config/ConfigParser.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigParser.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 17/11/2022.
+//
+
+import Foundation
+
+public enum ConfigParser {
+    
+    enum Keys {
+        
+        static let baseUrl = "BASE_URL"
+    }
+    
+}
