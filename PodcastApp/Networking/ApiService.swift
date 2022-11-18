1b4a24e2c667145a38399c013c2f6f5041faa139
Temi <ytemiloluwa@gmail.com>
2022-11-18 20:59:17 +0100

    [ Chore : Session Object ]

--- PodcastApp/Networking/ApiService.swift
+++ PodcastApp/Networking/ApiService.swift
@@ -10,6 +10,16 @@ import Combine
 
 public class APIService: ApiServiceProtocol {
     
+    // session object is being used to make http request to fetch data over the network
+    private var session: URLSession {
+        
+        let config = URLSessionConfiguration.default
+        config.urlCache = URLCache.shared
+        config.waitsForConnectivity = true
+        config.requestCachePolicy = .reloadRevalidatingCacheData
+        return URLSession(configuration: config, delegate: nil, delegateQueue: nil)
+    }
+    
     public func remotePublisher(term: String, limit: Int) -> AnyPublisher<Data, URLError> {
         <#code#>
     }
