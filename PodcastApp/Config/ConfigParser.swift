0a11c5ac2c5cd2267d1c7a334b77a6d242c175b5
Temi <ytemiloluwa@gmail.com>
2022-11-18 08:39:59 +0100

    [ Chore : InfoDictionary & baseUrl ]

--- PodcastApp/Config/ConfigParser.swift
+++ PodcastApp/Config/ConfigParser.swift
@@ -14,4 +14,30 @@ public enum ConfigParser {
         static let baseUrl = "BASE_URL"
     }
     
+    private static let infoDictionary: [String: Any] = {
+        
+        guard let dict = Bundle.main.infoDictionary else {
+            
+            fatalError("Plist file not found ")
+        }
+        
+        return dict
+    }()
+    
+    static let baseUrl: URL = {
+        
+        guard let baseUrlString = infoDictionary[Keys.baseUrl] as? String else {
+            
+            fatalError("BASE_URL not available in plist file")
+            
+        }
+        
+        guard let url = URL(string: baseUrlString) else {
+            
+            fatalError("Invalid BASE_URL")
+        }
+        
+        return url
+    }()
+    
 }
