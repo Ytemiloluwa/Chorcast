546368210723c95f3baddbb9265a251e5302677d
Temi <ytemiloluwa@gmail.com>
2022-11-18 09:03:28 +0100

    [ Chore : Tidying ConfigParser properties ]

--- PodcastApp/Config/ConfigParser.swift
+++ PodcastApp/Config/ConfigParser.swift
@@ -16,26 +16,16 @@ public enum ConfigParser {
     
     private static let infoDictionary: [String: Any] = {
         
-        guard let dict = Bundle.main.infoDictionary else {
-            
-            fatalError("Plist file not found ")
-        }
+        guard let dict = Bundle.main.infoDictionary else { fatalError("Plist file not found ") }
         
         return dict
     }()
     
     static let baseUrl: URL = {
         
-        guard let baseUrlString = infoDictionary[Keys.baseUrl] as? String else {
-            
-            fatalError("BASE_URL not available in plist file")
-            
-        }
+        guard let baseUrlString = infoDictionary[Keys.baseUrl] as? String else { fatalError("BASE_URL not available in plist file") }
         
-        guard let url = URL(string: baseUrlString) else {
-            
-            fatalError("Invalid BASE_URL")
-        }
+        guard let url = URL(string: baseUrlString) else { fatalError("Invalid BASE_URL") }
         
         return url
     }()
