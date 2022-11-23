2d005ede25c2de7873452a193d5245a1ecbbd808
Temi <ytemiloluwa@gmail.com>
2022-11-24 00:01:27 +0100

    [ Chore : AppState ]

--- PodcastApp/Store/AppState.swift
+++ PodcastApp/Store/AppState.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum AppState {
+    
+    enum Result<T: Equatable>: Equatable {
+        
+        static func ==(lhs: AppState.Result<T>, rhs: AppState.Result<T>) -> Bool {
+            
+            switch lhs {
+                
+            case.loading:
+                if case loading = rhs {
+                    
+                    return true
+                }
+            case.success(let lhsValue):
+                if case success(let rhsValue) = rhs {
+                    
+                    return rhsValue == lhsValue
+                }
+                
+            case.failure:
+                if case failure = rhs {
+                    
+                    return true
+                }
+                
+            }
+            
+            return false
+        }
+        case loading
+        case success(T)
+        case failure
+        
+    }
+    
+    struct Api {
+        
+        var preferredPodcast: [String: Result<[PodcastViewModel]>] = [:]
+       // var allGenres : Result<[GenreViewModel]> = .loading
+    }
+    
+}
