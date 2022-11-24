e754a031dedaf044643a63711006450f42c021f2
Temi <ytemiloluwa@gmail.com>
2022-11-25 00:36:22 +0100

    [ Chore : StateCompletion & Execution for ApiReducer ]

--- PodcastApp/Store/ApiReducer.swift
+++ PodcastApp/Store/ApiReducer.swift
@@ -10,5 +10,21 @@ import Combine
 
 enum ApiReducer {
     
+    typealias StateCompletion = ((AppState.Api) -> Void)
     
+    static func execute(action: Action.Api, state: AppState.Api, enviroment: AppEnviroment, completion: @escaping StateCompletion){
+        
+        switch action {
+            
+        case.fetchPodcasts(let term, let limit):
+            
+            let publisher = enviroment.api.remotePublisher(term: term, limit: limit)
+            
+        case.fetchGenres(let filename):
+            let publisher = enviroment.api.localPublisher(filename: filename)
+            
+        }
+    
+      // return action
+    }
 }
