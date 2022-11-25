17370eaf52e070e748fa3d43ac53d538fbd48498
Temi <ytemiloluwa@gmail.com>
2022-11-25 13:12:46 +0100

    [ Chore : Subscribe Function ]

--- PodcastApp/Store/ApiReducer.swift
+++ PodcastApp/Store/ApiReducer.swift
@@ -12,7 +12,7 @@ enum ApiReducer {
     
     typealias StateCompletion = ((AppState.Api) -> Void)
     
-    static func execute(action: Action.Api, state: AppState.Api, enviroment: AppEnviroment, completion: @escaping StateCompletion){
+    static func execute(action: Action.Api, state: AppState.Api, enviroment: AppEnviroment, completion: @escaping StateCompletion) -> AnyCancellable{
         
         switch action {
             
@@ -20,11 +20,53 @@ enum ApiReducer {
             
             let publisher = enviroment.api.remotePublisher(term: term, limit: limit)
             
+            return suscribe(to: publisher, type: PodcastList.self, enviroment: enviroment) { result, _ in
+                
+               
+            }
+            
+            
+            
         case.fetchGenres(let filename):
             let publisher = enviroment.api.localPublisher(filename: filename)
             
+            
+            return suscribe(to: publisher, type: GenreList.self, enviroment: enviroment) { result, _ in
+                
+                
+                
+            }
+            
         }
+        
+        
+    }
     
-      // return action
+    private static func suscribe<T: Codable>(to publisher: AnyPublisher<Data, URLError>, type: T.Type, enviroment: AppEnviroment, completion: @escaping (T?, Error?) -> Void) -> AnyCancellable {
+        
+        
+        publisher
+            .decode(type: type, decoder: enviroment.decoder)
+            .receive(on: DispatchQueue.main)
+            .sink { status in
+                
+                if case .failure(let error) = status {
+                    
+                    Log.error(error)
+                    completion(nil, error)
+                }
+            }receiveValue: { result in
+                
+                completion(result, nil )
+                
+            }
+        
+    }
+    
+ 
+    
+    private static func setPreferredPodcasts() {
+        
+        
     }
 }
