28286d018239771cdce0a21df0b8eabb372143db
Temi <ytemiloluwa@gmail.com>
2022-11-28 14:43:39 +0100

    [ Chore : Checking for Api integration ]

--- PodcastApp/ContentView.swift
+++ PodcastApp/ContentView.swift
@@ -8,16 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
- 
-    @StateObject private var internetMonitor = InternetMonitor()
+    
+    //@StateObject private var internetMonitor = InternetMonitor()
+    @StateObject private var store = Store()
+    private let term = "sport"
     
     var body: some View {
-        
-        Text(internetMonitor.isAvailable ? "Connected" : "Not Connected")
-        
-            .bold()
-            .foregroundColor(internetMonitor.isAvailable ? .green : .red)
-  
+    
+        List {
+
+            switch store.apiState.preferredPodcasts[term] {
+                
+            case .loading:
+                Text("Loading...")
+            case .success(let data):
+                ForEach(data) { podcast in
+                    
+                    Text(podcast.title)
+                }
+            case .failure:
+                Text("Failure")
+            case .none:
+                Text("Loading...")
+                
+            }
+        }
+        .onAppear {
+            
+            store.dispatch(.api(.fetchPodcasts(term, limit: 10)))
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
