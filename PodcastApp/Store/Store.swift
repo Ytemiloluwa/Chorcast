e456e7d25deb1b90be0db5f5625bc4824e24cd88
Temi <ytemiloluwa@gmail.com>
2022-11-28 08:28:43 +0100

    [ Chore : Dispatch function ]

--- PodcastApp/Store/Store.swift
+++ PodcastApp/Store/Store.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 class Store: ObservableObject {
-    
+    // notify views that will use it whenever it changes
     @Published private(set) var apiState = AppState.Api()
     
     private var subscription = Set<AnyCancellable>()
@@ -19,6 +19,19 @@ class Store: ObservableObject {
     init(enviroment: AppEnviroment = AppEnviroment()) {
         
         self.enviroment = enviroment
+    }
+    
+    func dispatch(_ action: Action) {
+        
+        switch action {
+            
+        case.api(let action):
+            // calling appropriate reducer based on the action emmited by views
+            ApiReducer.execute(action: action, state: apiState, enviroment: enviroment) { [weak self] state in
+                
+                self?.apiState = state
+            }.store(in: &subscription)
+        }
     }
     
 }
