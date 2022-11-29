76ab70afb0d8a13e7e328b989fe32ec0d9f82a99
Temi <ytemiloluwa@gmail.com>
2022-11-29 15:18:24 +0100

    [ Chore : Color+ ]

---
+++ PodcastApp/Extension/Color+.swift
@@ -0,0 +1,27 @@
+//
+//  Color+.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 29/11/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let pop = Color("pop")
+    static let backgroundColor = Color("backgroundColor")
+    static let overlay = Color("overlay")
+    static let text = Color("text")
+    static let shadow = Color("shadow")
+    
+    #if os(iOS)
+    static let body = Color(UIColor.secondaryLabel)
+    static let border = Color(UIColor.separator)
+    
+    #elseif os(macOS)
+    static let body = Color(NSColor.secondaryLabelColor)
+    static let border = Color(Nscolor.separatorColor)
+    #endif
+}
