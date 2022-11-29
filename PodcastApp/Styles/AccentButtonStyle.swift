718a0f0cf221c9520221a90b9f0e7aef293dae8c
Temi <ytemiloluwa@gmail.com>
2022-11-29 15:05:20 +0100

    [ Chore : AccentButtonStyle ]

--- PodcastApp/Styles/AccentButtonStyle.swift
+++ PodcastApp/Styles/AccentButtonStyle.swift
@@ -5,9 +5,24 @@
 //  Created by Temiloluwa on 29/11/2022.
 //
 
-import Foundation
+import SwiftUI
 
 struct AccentButtonStyle: ButtonStyle {
     
+    var verticalPadding: CGFloat
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+    
+        let horizontalPadding = verticalPadding * 2
+       
+        return configuration.label
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+           // .foregroundColor(Color.background)
+            .background(Color.accentColor)
+            .cornerRadius(15)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+        
+    }
     
 }
