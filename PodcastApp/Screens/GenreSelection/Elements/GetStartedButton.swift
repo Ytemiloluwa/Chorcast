37d0aaf42caf71ab6a5ec528ac10c7995db9d4dc
Temi <ytemiloluwa@gmail.com>
2022-11-30 15:59:16 +0100

    [ Chore : GetStartedButton ]

---
+++ PodcastApp/Screens/GenreSelection/Elements/GetStartedButton.swift
@@ -0,0 +1,25 @@
+//
+//  GetStartedButtion.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 30/11/2022.
+//
+
+import SwiftUI
+
+struct GetStartedButton: View {
+    
+    var action = {}
+    var body: some View {
+        Button(action: action, label: {
+            
+            Text("Get started")
+        })
+    }
+}
+
+struct GetStartedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        GetStartedButton()
+    }
+}
