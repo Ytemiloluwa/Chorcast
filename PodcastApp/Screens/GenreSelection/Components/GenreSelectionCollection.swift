73d1b22e239ec92aee273c0fd0f6c1ff38c0472a
Temi <ytemiloluwa@gmail.com>
2022-12-01 08:10:22 +0100

    [ Chore : creategenreToggle ]

--- PodcastApp/Screens/GenreSelection/Components/GenreSelectionCollection.swift
+++ PodcastApp/Screens/GenreSelection/Components/GenreSelectionCollection.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct GenreSelectionCollection: View {
+    
+    var genres: [GenreViewModel]
+    @Binding var selectedGenres: [String : GenreViewModel]
+    
+    private let gridItems = Array(repeating: GridItem.init(.adaptive(minimum: 40)), count: 1)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+            LazyHGrid(rows: gridItems, alignment: .center, spacing: 10) {
+                
+                ForEach(genres) { genre in
+                   creategenreToggle(genre)
+                }
+            }.padding()
+        }.frame(height: 340, alignment: .center)
+    }
+    
+    fileprivate func creategenreToggle(_ genre: GenreViewModel) -> some View {
+        
+        return GenreToggle(genre: genre) { selected in
+            
+            if selected {
+                
+                selectedGenres[genre.name] = genre
+            }else {
+                selectedGenres.removeValue(forKey: genre.name)
+                
+            }
+        }
     }
 }
-
 struct GenreSelectionCollection_Previews: PreviewProvider {
     static var previews: some View {
-        GenreSelectionCollection()
+        GenreSelectionCollection(genres: GenreViewModel.listPlaceHolders, selectedGenres: .constant([:]))
     }
 }
