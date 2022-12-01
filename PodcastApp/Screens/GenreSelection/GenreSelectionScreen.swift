dd0cf3e8e146231aaec745688a9d8929a4477157
Temi <ytemiloluwa@gmail.com>
2022-12-01 13:41:42 +0100

    [ Chore : GenreSelectionScreen ]

---
+++ PodcastApp/Screens/GenreSelection/GenreSelectionScreen.swift
@@ -0,0 +1,39 @@
+//
+//  GenreSelectionScreen.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 01/12/2022.
+//
+
+import SwiftUI
+
+struct GenreSelectionScreen: View {
+    @EnvironmentObject private var store: Store
+    var body: some View {
+        
+        switch store.apiState.allGenres {
+            
+        case.loading:
+            GenreSelectionContent(genres: GenreViewModel.listPlaceHolders) { _ in
+            }.onAppear(perform: {
+                
+                store.dispatch(.api(.fetchGenres(_filename: "genres")))
+            }).redacted(reason: .placeholder)
+        case.success(let data):
+            GenreSelectionContent(genres: data) { _ in
+                
+                
+            }
+        case.failure:
+             Text("Failed to Load genres...")
+        }
+    }
+}
+
+struct GenreSelectionScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        GenreSelectionScreen()
+            .previewLayout(.sizeThatFits)
+            .environmentObject(Store())
+    }
+}
