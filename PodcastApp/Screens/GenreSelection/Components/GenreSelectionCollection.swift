4a9e405c1cebf8203a950e7248bfea4448f34a88
Temi <ytemiloluwa@gmail.com>
2022-12-01 07:22:41 +0100

    [ Chore : GenreSelectionCollection ]

---
+++ PodcastApp/Screens/GenreSelection/Components/GenreSelectionCollection.swift
@@ -0,0 +1,20 @@
+//
+//  GenreSelectionCollection.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 01/12/2022.
+//
+
+import SwiftUI
+
+struct GenreSelectionCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GenreSelectionCollection_Previews: PreviewProvider {
+    static var previews: some View {
+        GenreSelectionCollection()
+    }
+}
