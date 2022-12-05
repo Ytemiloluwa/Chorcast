ad6462edd0b3464c4cc851715c286e031352e7c7
Temi <ytemiloluwa@gmail.com>
2022-12-05 07:57:59 +0100

    [ Chore : Error Fix ]

--- PodcastApp/CoreData/CoreDataServiceProtocol.swift
+++ PodcastApp/CoreData/CoreDataServiceProtocol.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public protocol CoreDataserviceProtocol {
+protocol CoreDataserviceProtocol {
     
     func saveGenres(_ genres: [GenreViewModel]) -> Bool
     func fetchGenres() -> [GenreViewModel]
