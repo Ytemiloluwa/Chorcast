6e86b9a3d7df906caea6b96d6ab9df4b886a3889
Temi <ytemiloluwa@gmail.com>
2022-12-06 14:02:07 +0100

    [ Chore : MenuItem properties ]

--- PodcastApp/Models/MenuItem.swift
+++ PodcastApp/Models/MenuItem.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct MenuItem: Identifiable, Equatable {
+    
+    var id: String { name }
+    var name = ""
+    var icon = ""
+    
+    static var placeholder: MenuItem {
+        
+        MenuItem(name: "Item", icon: "circlebadge.fill")
+    }
+    static var all: [MenuItem] {
+        
+        [MenuItem(name: "Profile", icon: "person.crop.cirlce.fill"), MenuItem(name: "Search", icon: "magniflyingglass.circle.fill"), MenuItem(name: "Downloaded", icon: "sqaure.and.arrow.down.fill"), MenuItem(name: "Bookmarked", icon: "bookmark.fill")]
+    }
+}
