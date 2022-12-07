6ba0a2a51f37a41e6b0c47a57d442d92fd6d8051
Temi <ytemiloluwa@gmail.com>
2022-12-07 15:42:34 +0100

    [ Chore : HomeGenresCollectionComponent ]

---
+++ PodcastApp/Screens/Home/Components/HomeGenresCollectionComponent.swift
@@ -0,0 +1,36 @@
+//
+//  HomeGenresCollectionComponent.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 07/12/2022.
+//
+
+import SwiftUI
+
+struct HomeGenresCollectionComponent: View {
+    
+    var genres: [GenreViewModel]
+    
+    private let gridItems: [GridItem] = Array(repeating: .init(.adaptive(minimum: 40), spacing: 20), count: 2)
+    
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: true) {
+            
+            LazyHGrid(rows: gridItems, alignment: .center, spacing: 20) {
+                
+                ForEach(genres) { genre in
+                    
+                    GenreLinkView(genre: genre)
+                    
+                }
+            }.padding(.horizontal)
+        }.frame(height: 180, alignment: .leading)
+    }
+}
+
+struct HomeGenresCollectionComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeGenresCollectionComponent(genres: GenreViewModel.listPlaceHolders)
+    }
+}
