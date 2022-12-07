4cd6975b44f5a915b86adf83ee0ab2d2342e00ab
Temi <ytemiloluwa@gmail.com>
2022-12-07 16:42:28 +0100

    [ Chore : HomeGenresContent ]

---
+++ PodcastApp/Screens/Home/Components/HomeGenresContent.swift
@@ -0,0 +1,59 @@
+//
+//  HomeGenresContentComponent.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 07/12/2022.
+//
+
+import SwiftUI
+
+struct HomeGenresContent: View {
+    
+    @EnvironmentObject private var store: Store
+    
+    var body: some View {
+        
+        switch store.apiState.allGenres {
+            
+        case.loading:
+            createContent(genres: GenreViewModel.listPlaceHolders)
+                .redacted(reason: .placeholder)
+                .onAppear {
+                    
+                    store.dispatch(.api(.fetchGenres(_filename: "genres")))
+                }
+            
+        case.success(let genres):
+            createContent(genres: genres)
+            
+        case.failure:
+            Text("Failed to Load Genres")
+        }
+    }
+    
+    private func createContent(genres: [GenreViewModel]) -> some View {
+        
+        VStack {
+            
+            HomeSectionHeaderView(title: "All genres")
+            HomeGenresCollection(genres: genres)
+        }
+    }
+}
+
+struct HomeGenresContent_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            
+            HomeGenresContent()
+//                .background(Color.background)
+                .previewLayout(.sizeThatFits)
+//                .colorScheme(.dark)
+//            HomeGenresContent()
+//                .background(Color.background)
+//                .previewLayout(.sizeThatFits)
+//                .colorScheme(.light)
+            
+        }.environmentObject(Store())
+    }
+}
