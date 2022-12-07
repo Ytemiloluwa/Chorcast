f83e9e2fdc77658e6ef847c66b1e107a6ab049c4
Temi <ytemiloluwa@gmail.com>
2022-12-07 14:19:32 +0100

    [ Chore : GenreLinkView ]

---
+++ PodcastApp/Screens/GenreSelection/Components/GenreLinkView.swift
@@ -0,0 +1,38 @@
+//
+//  GenreLinkView.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 07/12/2022.
+//
+
+import SwiftUI
+
+struct GenreLinkView: View {
+    var genre: GenreViewModel
+    var body: some View {
+ 
+        NavigationLink(destination: Text("To be implmented"), label: {
+            
+            GenreView(genre: genre)
+                .frame(minWidth: 250, maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                .background(Color.overlay)
+                .cornerRadius(15)
+        })
+    }
+}
+
+struct GenreLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            
+            GenreLinkView(genre: GenreViewModel.placeHolder)
+                .frame(width: 250, height: 70, alignment: .leading)
+                .previewLayout(.sizeThatFits)
+            
+            GenreLinkView(genre: GenreViewModel.placeHolder)
+                .frame(width: 250, height: 70, alignment: .leading)
+                .colorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
