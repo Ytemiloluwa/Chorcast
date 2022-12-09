7096cd136ae0039c94cf086a635dd7d1b5f0763c
Temi <ytemiloluwa@gmail.com>
2022-12-09 07:50:59 +0100

    [ Chore : Caching Update ]

--- PodcastApp/Utils/Cache.swift
+++ PodcastApp/Utils/Cache.swift
@@ -8,13 +8,113 @@
 import Foundation
 
 final class Cache<Key: Hashable, Value> {
+    private let wrapped = NSCache<WrappedKey, Entry>()
+    private let dateProvider: () -> Date
+    private let entryLifetime: TimeInterval
+    private let keyTracker = KeyTracker()
     
-    private let dateprovider: () -> Date
-    private let entryLifeTime: TimeInterval
-    
-    init(dateprovider: @escaping () -> Date = Date.init, entryLifeTime: TimeInterval = 12 * 60 * 60) {
+    subscript(key: Key) -> Value? {
+        get {
+            return value(forKey: key)
+        }
         
-        self.dateprovider = dateprovider
-        self.entryLifeTime = entryLifeTime
+        set {
+            guard let value = newValue else {
+                remove(forKey: key)
+                return
+            }
+            
+            insert(value, forKey: key)
+        }
+    }
+    
+    init(dateProvider: @escaping () -> Date = Date.init,
+         entryLifetime: TimeInterval = 12 * 60 * 60, maximumEntryCount: Int = 50) {
+        self.dateProvider = dateProvider
+        self.entryLifetime = entryLifetime
+        wrapped.countLimit = maximumEntryCount
+        wrapped.delegate = keyTracker
+    }
+    
+    func insert(_ value: Value, forKey key: Key) {
+        let date = dateProvider().addingTimeInterval(entryLifetime)
+        let entry = Entry(key: key, value: value, expirationDate: date)
+        insert(entry)
+    }
+    
+    func value(forKey key: Key) -> Value? {
+        entry(forKey: key)?.value
+    }
+    
+    func remove(forKey key: Key) {
+        wrapped.removeObject(forKey: WrappedKey(key))
     }
 }
+
+private extension Cache {
+    func entry(forKey key: Key) -> Entry? {
+        guard let entry = wrapped.object(forKey: WrappedKey(key)) else {
+            return nil
+        }
+
+        guard dateProvider() < entry.expirationDate else {
+            remove(forKey: key)
+            return nil
+        }
+
+        return entry
+    }
+
+    func insert(_ entry: Entry) {
+        wrapped.setObject(entry, forKey: WrappedKey(entry.key))
+        keyTracker.keys.insert(entry.key)
+    }
+}
+
+private extension Cache {
+    final class KeyTracker: NSObject, NSCacheDelegate {
+        var keys = Set<Key>()
+
+        func cache(_ cache: NSCache<AnyObject, AnyObject>,
+                   willEvictObject object: Any) {
+            guard let entry = object as? Entry else {
+                return
+            }
+
+            keys.remove(entry.key)
+        }
+    }
+}
+
+private extension Cache {
+    final class Entry {
+        let key: Key
+        let value: Value
+        let expirationDate: Date
+
+        init(key: Key, value: Value, expirationDate: Date) {
+            self.key = key
+            self.value = value
+            self.expirationDate = expirationDate
+        }
+    }
+}
+
+private extension Cache {
+    final class WrappedKey: NSObject {
+        let key: Key
+
+        init(_ key: Key) { self.key = key }
+
+        override var hash: Int { return key.hashValue }
+
+        override func isEqual(_ object: Any?) -> Bool {
+            guard let value = object as? WrappedKey else {
+                return false
+            }
+
+            return value.key == key
+        }
+    }
+}
+
