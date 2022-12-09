e617d502506576970a3653de25e8c742efcae484
Temi <ytemiloluwa@gmail.com>
2022-12-09 08:46:17 +0100

    [ Chore : ImageCaching ]

--- PodcastApp/Utils/ImageCache.swift
+++ PodcastApp/Utils/ImageCache.swift
@@ -9,5 +9,24 @@ import Foundation
 
 struct ImageCache {
     
+    static var shared = ImageCache()
+    
+    private let cache: Cache<String, Data>
+    
+    
+    init(lifetime: TimeInterval = 12 * 60 * 60, maximumCount: Int = 200) {
+        
+        cache = Cache<String, Data>(entryLifetime: lifetime, maximumEntryCount: maximumCount)
+    }
+    
+    func set(_ data: Data, for key: String) {
+        
+        cache.insert(data, forKey: key as String)
+    }
+    
+    func get(for key: String) -> Data? {
+        
+        cache.value(forKey: key as String)
+    }
     
 }
