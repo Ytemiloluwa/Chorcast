2613de4ba4f9f13e92de706a8d1434f1d9493c76
Temi <ytemiloluwa@gmail.com>
2022-12-15 12:15:09 +0100

    [ Chore : Added HomeLibrarySection ]

---
+++ PodcastApp/Screens/Home/Components/HomeMyLibrarySection.swift
@@ -0,0 +1,46 @@
+//
+//  HomeMyLibrarySection.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 15/12/2022.
+//
+
+import SwiftUI
+
+struct HomeMyLibrarySection: View {
+    @State private var selectedGenre = ""
+    
+    private var section : some View {
+        
+        Section(header: HomeMyLibrarySectionHeader(seleted: $selectedGenre)) {
+            
+            PodcastListContent(genre: selectedGenre)
+        }
+    }
+    var body: some View {
+        
+        LazyVGrid(columns: Array(repeating: .init(.adaptive(minimum: 350), spacing: 20), count: 1), alignment: .leading, spacing: 10, pinnedViews: [.sectionHeaders], content: { section })
+            
+        
+        
+    }
+}
+
+struct HomeMyLibrarySection_Previews: PreviewProvider {
+    
+    static let store = Store(enviroment: AppEnvironment(api: previewApiService(), coredata: PreviewCoreDataService()))
+    
+    static var previews: some View {
+       
+        Group {
+            
+            HomeMyLibrarySection()
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+            
+            HomeMyLibrarySection()
+                .previewLayout(.sizeThatFits)
+            
+        }.environmentObject(store)
+    }
+}
