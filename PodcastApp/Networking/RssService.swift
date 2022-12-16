c64fed3e3f011611a471c1f046c7d9618559967b
Temi <ytemiloluwa@gmail.com>
2022-12-17 00:41:50 +0100

    [ Chore : Added RssService Class ]

---
+++ PodcastApp/Networking/RssService.swift
@@ -0,0 +1,18 @@
+//
+//  RssService.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 17/12/2022.
+//
+
+import Foundation
+import Combine
+import FeedKit
+
+class RssService: RssServiceProtocol {
+    
+    func publisher(urlString: String) -> AnyPublisher<FeedKit.Feed, FeedKit.ParserError> {
+        <#code#>
+    }
+    
+}
