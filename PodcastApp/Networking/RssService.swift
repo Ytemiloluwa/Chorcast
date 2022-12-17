2b87425b9646bfe9f337cb109b7d0c6380489d51
Temi <ytemiloluwa@gmail.com>
2022-12-17 07:25:29 +0100

    [ Chore : Updated Publisher Method ]

--- PodcastApp/Networking/RssService.swift
+++ PodcastApp/Networking/RssService.swift
@@ -12,7 +12,28 @@ import FeedKit
 class RssService: RssServiceProtocol {
     
     func publisher(urlString: String) -> AnyPublisher<FeedKit.Feed, FeedKit.ParserError> {
-        <#code#>
+        
+        guard let url = URL(string: urlString) else {
+            
+            return Fail(error: ParserError.internalError(reason: "Invalid url")).eraseToAnyPublisher()
+        }
+        
+        let parser = FeedParser(URL: url)
+        
+        return Future<Feed, ParserError> { promise in
+            
+            parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { result in
+                
+                switch result {
+                    
+                case.success(let feed):
+                    promise(.success(feed))
+                case.failure(let error):
+                    promise(.failure(error))
+                }
+            }
+            
+        }.eraseToAnyPublisher()
     }
     
 }
