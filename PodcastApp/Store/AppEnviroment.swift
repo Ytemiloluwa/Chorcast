3348f414e65e818948c3d32d9a410e2b1c4494fe
Temi <ytemiloluwa@gmail.com>
2022-12-17 23:02:32 +0100

    [ Chore : Updated AppEnvironment ]

--- PodcastApp/Store/AppEnviroment.swift
+++ PodcastApp/Store/AppEnviroment.swift
@@ -12,12 +12,14 @@ struct AppEnvironment {
     let decoder: JSONDecoder
     let api: ApiServiceProtocol
     let coredata: CoreDataserviceProtocol
+    let rss: RssServiceProtocol
     
-    init(decoder: JSONDecoder = .init(), api: ApiServiceProtocol = APIService(), coredata: CoreDataserviceProtocol = CoreDataService()) {
+    init(decoder: JSONDecoder = .init(), api: ApiServiceProtocol = APIService(), coredata: CoreDataserviceProtocol = CoreDataService(), rss: RssServiceProtocol = RssService()) {
         
         self.api = api
         self.decoder = decoder
         self.coredata = coredata
+        self.rss = rss
     }
     
 }
