e1753d0db539972971c839d0e85ed20cc5c42e41
Temi <ytemiloluwa@gmail.com>
2022-12-18 00:24:32 +0100

    [ Chore : Added UpdatePodcast method to ApiReducer ]

--- PodcastApp/Store/ApiReducer.swift
+++ PodcastApp/Store/ApiReducer.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import FeedKit
 
 struct ApiReducer {
     typealias StateCompletion = ((AppState.Api) -> Void)
@@ -18,14 +19,28 @@ struct ApiReducer {
             
             return subscribe(to: publisher, type: PodcastList.self, environment: environment) { result, _ in
                 setPreferredPodcasts(state: state, term: term, result: result, completion: completion)
-                   }
-                   
+            }
+            
         case .fetchGenres(let filename):
             let publisher = environment.api.localPublisher(filename: filename)
-                    return subscribe(to: publisher, type: GenreList.self, environment: environment) { result, _ in
-                        setAllGenres(state: state, result: result, completion: completion)
-                    }
-
+            return subscribe(to: publisher, type: GenreList.self, environment: environment) { result, _ in
+                setAllGenres(state: state, result: result, completion: completion)
+            }
+            
+        case .updatePodcasts(let podcast):
+            
+            return update(podcast, environment: environment) { podcast, error in
+                
+                var copyState = state
+                
+                if error != nil {
+                    
+                    copyState.podcast = .failure
+                }else {
+                    
+                    copyState.podcast = .success(podcast)
+                }
+            }
         }
     }
     
@@ -47,17 +62,17 @@ struct ApiReducer {
     private static func setPreferredPodcasts( state: AppState.Api, term: String, result: PodcastList?, completion:  @escaping StateCompletion) {
         var copyState = state
         
-          if let podcastList = result {
-              copyState.preferredPodcasts[term] = .success(podcastList.results
-                                                          .map { PodcastViewModel(podcast: $0) }
-                                                          .filter { !$0.feedUrl.isEmpty && $0.totalEpisodes > 0 })
-          } else {
-              copyState.preferredPodcasts[term] = .failure
-          }
-          
-          completion(copyState)
+        if let podcastList = result {
+            copyState.preferredPodcasts[term] = .success(podcastList.results
+                .map { PodcastViewModel(podcast: $0) }
+                .filter { !$0.feedUrl.isEmpty && $0.totalEpisodes > 0 })
+        } else {
+            copyState.preferredPodcasts[term] = .failure
+        }
+        
+        completion(copyState)
     }
-
+    
     private static func setAllGenres(state: AppState.Api, result: GenreList?, completion:  @escaping StateCompletion) {
         var copyState = state
         if let genreList = result {
@@ -67,5 +82,29 @@ struct ApiReducer {
         }
         
         completion(copyState)
+    }
+    
+    static private func update(_ podcast: PodcastViewModel, environment: AppEnvironment, completion: @escaping (PodcastViewModel, Error?) -> Void) -> AnyCancellable {
+        
+        environment.rss.publisher(urlString: podcast.feedUrl)
+            .receive(on: DispatchQueue.main)
+            .sink { status in
+                if case.failure(let error) = status {
+                    
+                    completion(podcast, error)
+                }
+            } receiveValue: { feed in
+                
+                guard let rssFeed = feed.rssFeed else {
+                    
+                    completion(podcast, ParserError.internalError(reason: "Invalid error"))
+                    
+                    return
+                }
+                
+                let updatedPodcast = PodcastViewModel(rssFeed, existingPodcast: podcast)
+                completion(updatedPodcast, nil)
+                
+        }
     }
 }
