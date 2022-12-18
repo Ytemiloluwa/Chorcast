961c8b0ce588bbc1ae15decf58d191a8fa489752
Temi <ytemiloluwa@gmail.com>
2022-12-18 21:36:21 +0100

    [ Chore : PodcastDetailsScreen ]

---
+++ PodcastApp/Screens/PodcastDetails/PodcastDetailsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  PodcastDetailsScreen.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 18/12/2022.
+//
+
+import SwiftUI
+
+struct PodcastDetailsScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PodcastDetailsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PodcastDetailsScreen()
+    }
+}
