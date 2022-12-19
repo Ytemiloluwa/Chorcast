641fe6cb10a689d175429b9100316fab8682dd30
Temi <ytemiloluwa@gmail.com>
2022-12-19 01:46:18 +0100

    [ Chore : Updated PodcastDetailsScreen ]

--- PodcastApp/Screens/PodcastDetails/PodcastDetailsScreen.swift
+++ PodcastApp/Screens/PodcastDetails/PodcastDetailsScreen.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct PodcastDetailsScreen: View {
+    
+    var podcast: PodcastViewModel
+    @EnvironmentObject private var store: Store
+    @State private var state: AppState.Result<PodcastViewModel> = .loading
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        switch state {
+            
+        case.loading:
+            Text("Loading...")
+        case.success(let podcast):
+            Text(podcast.title)
+        case.failure:
+            FailureView(message: "Unable to fetchPodcast", retryAction: checkState)
+        }
+    }
+    
+    private func checkState() {
+        
+        if case.success(let podcast) = store.apiState.podcast {
+            
+            if podcast.title == self.podcast.title {
+                
+                state = store.apiState.podcast
+            }else {
+                
+                store.dispatch(.api(.updatePodcasts(podcast)))
+            }
+        }
     }
 }
 
 struct PodcastDetailsScreen_Previews: PreviewProvider {
+    
+    static let store = Store(enviroment: AppEnvironment(api: previewApiService(), coredata: PreviewCoreDataService()))
     static var previews: some View {
-        PodcastDetailsScreen()
+        Group {
+            PodcastDetailsScreen(podcast: PodcastViewModel.placeHolder)
+                .preferredColorScheme(.dark)
+            
+            PodcastDetailsScreen(podcast: PodcastViewModel.placeHolder)
+            
+        }.environmentObject(store)
     }
 }
