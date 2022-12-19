644713753af50766ea7058d6a094ea12753d7d46
Temi <ytemiloluwa@gmail.com>
2022-12-19 19:56:30 +0100

    [ Chore : Updated PodcastViewModel ]

--- PodcastApp/ViewModels/PodcastViewModel.swift
+++ PodcastApp/ViewModels/PodcastViewModel.swift
@@ -59,8 +59,8 @@ struct PodcastViewModel: Identifiable, Equatable {
                          author: "Lorem ipsum",
                          totalEpisodes: 23,
                          explicitContent: "NO",
-                         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                         country: "NG",
+                         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                         country: "US",
                          genres: ["Technology", "NFL", "Comedy", "Politics", "Geography"],
                          nextEpisodePubDate: Date(),
                          feedUrl: "")
