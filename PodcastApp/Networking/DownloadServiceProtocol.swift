006890ca2c0ff36b63a4dde050444f61e4b060ff
Temi <ytemiloluwa@gmail.com>
2022-12-20 15:06:04 +0100

    [ Chore : Added DownloadServiceProtocol ]

---
+++ PodcastApp/Networking/DownloadServiceProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadServiceProtocol.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 20/12/2022.
+//
+
+import Foundation
+
+
+protocol DownloadServiceProtocol: URLSessionDownloadDelegate {
+    
+    var progress: ((CGFloat) -> Void) { get set }
+    var didFinish: ((Data?) -> Void) { get set }
+    
+    func download(from url: URL)
+    func cancel()
+}
