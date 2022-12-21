ede906f3ea7952d583e1c17c390d5e6d2e228312
Temi <ytemiloluwa@gmail.com>
2022-12-21 21:48:39 +0100

    [ Chore : Updated PreviewCoreDataService ]

--- PodcastApp/CoreData/previewCoreDataService.swift
+++ PodcastApp/CoreData/previewCoreDataService.swift
@@ -9,6 +9,11 @@ import Foundation
 import CoreData
 
 class PreviewCoreDataService: CoreDataserviceProtocol {
+    func saveEpisode(_ episode: EpisodeViewModel, audio: Data) -> Bool {
+        
+        return false
+    }
+    
     
     func saveGenres(_ genres: [GenreViewModel]) -> Bool {
         return true
