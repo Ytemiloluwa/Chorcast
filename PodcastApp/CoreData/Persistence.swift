ec8d6acd8f5a0af88701b17d3de03f5113cc1d46
Temi <ytemiloluwa@gmail.com>
2022-12-21 21:31:14 +0100

    [ Chore : Created StoreDescriptions for CloudSync & Local Configurations ]

--- PodcastApp/CoreData/Persistence.swift
+++ PodcastApp/CoreData/Persistence.swift
@@ -12,13 +12,31 @@ struct PersistenceController {
     let container: NSPersistentCloudKitContainer
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "PodcastApp")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
+        
+        let defaultDirectoryURL = NSPersistentContainer.defaultDirectoryURL()
+        
+        let cloudSyncStoreDescription = createDescription(configuration: "CloudSync", dirURl: defaultDirectoryURL)
+        let localStoreDescription = createDescription(configuration: "Local", dirURl: defaultDirectoryURL)
+        
+//        if inMemory {
+//            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+//        }
+        // override the default configuration by setting the persistentStoreDescriptions of the container giving it the two store descriptions created.
+        container.persistentStoreDescriptions = [cloudSyncStoreDescription, localStoreDescription]
         container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+    }
+    
+    private func createDescription(configuration: String, dirURl: URL) -> NSPersistentStoreDescription {
+        
+        let dbUrl = dirURl.appendingPathComponent("\(configuration).sqlite")
+        let storeDescription = NSPersistentStoreDescription(url: dbUrl)
+        storeDescription.configuration = configuration
+        
+        return storeDescription
+        
     }
 }
