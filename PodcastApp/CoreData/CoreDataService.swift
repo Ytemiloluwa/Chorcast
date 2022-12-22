c34a6b17727dba316c69aa67ef97062ba9386cdc
Temi <ytemiloluwa@gmail.com>
2022-12-22 22:42:08 +0100

    [ Chore : Updated EpisodeManagedObject Att

--- PodcastApp/CoreData/CoreDataService.swift
+++ PodcastApp/CoreData/CoreDataService.swift
@@ -10,40 +10,9 @@ import CoreData
 
 class CoreDataService: CoreDataserviceProtocol {
     
-    func saveEpisode(_ episode: EpisodeViewModel, audio: Data) -> Bool {
-        return false
-    }
-    
     private var context: NSManagedObjectContext {
         
         return PersistenceController.shared.container.viewContext
-    }
-    
-    func saveGenres(_ genres: [GenreViewModel]) -> Bool {
-        
-        genres.forEach {
-            
-            let newGenres = GenreManagedObject(context: context)
-            newGenres.id = Int64($0.id)
-            newGenres.name = $0.name
-        }
-        return saveContext()
-    }
-    
-    func fetchGenres() -> [GenreViewModel] {
-        
-        let request: NSFetchRequest<GenreManagedObject> = GenreManagedObject.fetchRequest()
-        
-        do {
-            
-            let result = try context.fetch(request)
-            return result.map { GenreViewModel(genre: $0)}
-        } catch let error {
-            Log.error(error)
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            
-        }
     }
     
     private func saveContext() -> Bool {
@@ -62,6 +31,87 @@ class CoreDataService: CoreDataserviceProtocol {
         }
         
         return true
+    }
+
+    
+    func saveGenres(_ genres: [GenreViewModel]) -> Bool {
+        
+        genres.forEach {
+            
+            let newGenres = GenreManagedObject(context: context)
+            newGenres.id = Int64($0.id)
+            newGenres.name = $0.name
+        }
+        return saveContext()
+    }
+    
+    func saveEpisode(_ episode: EpisodeViewModel, audio: Data) -> Bool {
+        return false
+    }
+    
+    func fetchGenres() -> [GenreViewModel] {
+        
+        let request: NSFetchRequest<GenreManagedObject> = GenreManagedObject.fetchRequest()
+        
+        do {
+            
+            let result = try context.fetch(request)
+            return result.map { GenreViewModel(genre: $0)}
+        } catch let error {
+            Log.error(error)
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            
+        }
+    }
+    
+    func fetchEpisode(_ episode: EpisodeViewModel) -> EpisodeManagedObject? {
+        
+        return nil
+        
+        
+    }
+    
+    func fetchPodcast(_ id: Int) -> PodcastManagedObject? {
+        
+        return nil
+    }
+    
+    
+    private func createManagedEpisode(_ episode: EpisodeViewModel, audioData: Data) -> EpisodeManagedObject {
+        
+        let managedEpisode = EpisodeManagedObject(context: context)
+        managedEpisode.id = episode.id
+        managedEpisode.title = episode.title
+        managedEpisode.createdAt = Date()
+        managedEpisode.descr = episode.description
+        managedEpisode.duration = episode.audioLength
+        managedEpisode.releaseDate = episode.date.toDate()
+        managedEpisode.size = episode.audioSize
+        managedEpisode.soundAudioUrl = episode.audioUrl
+        managedEpisode.audio = audioData
+        return managedEpisode
+        
+    }
+    
+    private func createManagedPodcast(_ podcast: PodcastViewModel) -> PodcastManagedObject {
+        
+        let managedPodcast = PodcastManagedObject(context: context)
+        managedPodcast.title = podcast.title
+        managedPodcast.author = podcast.author
+        managedPodcast.createdAt = Date()
+        managedPodcast.country = podcast.country
+        managedPodcast.descr = podcast.description
+        managedPodcast.explicitContent = podcast.explicitContent
+        managedPodcast.feedUrl = podcast.feedUrl
+        managedPodcast.releaseDate = podcast.date.toDate()
+        managedPodcast.langauge = podcast.language
+        managedPodcast.publisher = podcast.publisher
+        managedPodcast.id = Int64(podcast.id)
+        managedPodcast.imageUrl = podcast.imageUrl
+        managedPodcast.thumbnailUrl = podcast.thumbnailUrl
+        managedPodcast.genres = podcast.genres.joined(separator: ",")
+        return managedPodcast
     }
     
 }
--- PodcastApp/CoreData/previewCoreDataService.swift
+++ PodcastApp/CoreData/previewCoreDataService.swift
@@ -9,6 +9,15 @@ import Foundation
 import CoreData
 
 class PreviewCoreDataService: CoreDataserviceProtocol {
+    func fetchEpisode(_ episode: EpisodeViewModel) -> EpisodeManagedObject? {
+        return nil
+    }
+    
+    func fetchPodcast(_ id: Int) -> PodcastManagedObject? {
+        
+        return nil
+    }
+    
     func saveEpisode(_ episode: EpisodeViewModel, audio: Data) -> Bool {
         
         return false
