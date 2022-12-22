e44d4352c93a1bf83a7b6f3a6a93299afc254edd
Temi <ytemiloluwa@gmail.com>
2022-12-23 00:08:58 +0100

    [ Chore : Updated EpisodeViewHeader ]

--- PodcastApp/Screens/EpiosdeList/Component/EpisodeViewHeader.swift
+++ PodcastApp/Screens/EpiosdeList/Component/EpisodeViewHeader.swift
@@ -10,30 +10,98 @@ import SwiftUI
 struct EpisodeViewHeader: View {
     
     var episode: EpisodeViewModel
-    var downloadEpisode = {}
+    
+    
+    @State private var state = DownloadManager.State.idle
+    @EnvironmentObject private var downloadManager: DownloadManager
+    
+    private var downloadButton: some View {
+        
+        Button(action: downloadEpisode, label: {
+            
+            Image(systemName: "icloud.and.arrow.down.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20, height: 20)
+                .foregroundColor(.border)
+        }).buttonStyle(BorderlessButtonStyle())
+    }
+    
+    private var pendingView: some View {
+        
+        Text("Pending...")
+            .font(.callout)
+            .foregroundColor(.gray)
+        
+    }
+    
+    private var savedImage: some View {
+        
+        Image(systemName: "clock.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 20, height: 20)
+            .foregroundColor(.border)
+    }
+    
     
     var body: some View {
-    
+        
         HStack {
-    
+            
             Text(episode.date)
             
             Spacer()
             
-            Button(action: downloadEpisode, label: {
-                
-                Image(systemName: "icloud.and.arrow.down.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(.border)
-            }).buttonStyle(BorderlessButtonStyle())
+            createDownloadButton()
         }
     }
     
-    private func downloadEpiosde() {
+    @ViewBuilder
+    
+    private func createDownloadButton() -> some View {
         
+        switch state {
+            
+        case .idle:
+            downloadButton
+            
+        case .pending:
+            pendingView
+            
+        case .downloading(let progress):
+            createProgressView(progress)
+            
+            
+        case .done(let success):
+            if success {
+                
+                savedImage
+                
+            }else {
+                
+                downloadButton
+            }
+            
+        }
+    }
+    
+    private func createProgressView(_ progress: CGFloat) -> some View {
         
+        Button(action: abort, label: {
+            
+            Text("\(progress)")
+        })
+    }
+    
+    private func abort() {
+        
+        downloadManager.abort(name: episode.title)
+    }
+    
+    private func downloadEpisode() {
+        
+        downloadManager.download(episode)
     }
 }
 
