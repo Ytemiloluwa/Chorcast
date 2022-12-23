c020aa7b0a6108e16f5e465cfcb345972ab6d86d
Temi <ytemiloluwa@gmail.com>
2022-12-23 21:52:07 +0100

    [ Chore : Added FilterEpisodes ]

--- PodcastApp/Screens/EpiosdeList/EpisodeListScreen.swift
+++ PodcastApp/Screens/EpiosdeList/EpisodeListScreen.swift
@@ -9,23 +9,25 @@ import SwiftUI
 
 struct EpisodeListScreen: View {
     
-    var episodes: [EpisodeViewModel]
+    var episodes: [EpisodeViewModel] = []
     
-    enum ListScreen: String, CaseIterable {
+    enum ListSection: String, CaseIterable {
         
         case downloaded = "Downloaded"
         case allEpisodes = "All episodes"
     }
-    @State private var listSection: ListScreen = .allEpisodes
+    @State private var listSection = ListSection.allEpisodes
+    @State private var filteredEpisodes = [EpisodeViewModel]()
     
     private let gridItems: [GridItem] = Array(repeating: .init(.adaptive(minimum: 350), spacing: 20), count: 1)
     
     private var section: some View {
         
         Section(header: createSegmentedPicker()) {
-            ForEach(episodes) { episode in
+            ForEach(filteredEpisodes) { episode in
                 
                 EpisodeView(episode: episode, play: {})
+                    .transition(.move(edge: .leading))
             }
         }
     }
@@ -34,20 +36,47 @@ struct EpisodeListScreen: View {
         
         ScrollView {
             
-            LazyVGrid(columns: gridItems, alignment: .leading, spacing: 20, content: {section}).padding()
+            LazyVGrid(columns: gridItems, alignment: .leading, spacing: 20, content: {section})
+                .padding()
             
-        }.navigationBarTitle("\(episodes.count) Episodes",displayMode: .inline)
+        }.onAppear(perform: filterEpisodes)
+            .onChange(of: listSection) { _ in
+                
+                filterEpisodes()
+            }
+            
+          //  .animation(.easeIn, value: listSection)
         
+        .navigationBarTitle("\(episodes.count) Episodes")
+        
+    }
+    
+    fileprivate func filterEpisodes() {
+        
+        switch listSection {
+        case .downloaded:
+            filteredEpisodes = episodes.filter({ episode in
+                
+                if let audio = episode.audio {
+                    
+                    return !audio.isEmpty
+                }
+                
+                return false
+            })
+        case .allEpisodes:
+            filteredEpisodes = episodes
+        }
     }
     
     func createSegmentedPicker() -> some View {
         
         Picker("", selection: $listSection) {
             
-            Text(ListScreen.allEpisodes.rawValue)
-                .tag(ListScreen.allEpisodes)
-            Text(ListScreen.downloaded.rawValue)
-                .tag(ListScreen.downloaded)
+            Text(ListSection.allEpisodes.rawValue)
+                .tag(ListSection.allEpisodes)
+            Text(ListSection.downloaded.rawValue)
+                .tag(ListSection.downloaded)
         }.pickerStyle(SegmentedPickerStyle())
             .padding(.vertical, 5)
             .background(Color.backgroundColor)
@@ -65,6 +94,7 @@ struct EpiosdeListScreen_Previews: PreviewProvider {
             EpisodeListScreen(episodes: EpisodeViewModel.listPlaceholder)
                 .preferredColorScheme(.light)
                 .previewLayout(.sizeThatFits)
-        }
+            
+        }.environmentObject(DownloadManager())
     }
 }
