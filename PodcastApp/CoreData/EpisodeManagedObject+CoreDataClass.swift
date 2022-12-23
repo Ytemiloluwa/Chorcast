40db90e2ee7cbc0dec794e8b4f3707df6fb7bf21
Temi <ytemiloluwa@gmail.com>
2022-12-23 15:44:32 +0100

    [ Chore : Added EpisodeManagedObject+CoreDataClass ]

---
+++ PodcastApp/CoreData/EpisodeManagedObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  EpisodeManagedObject+CoreDataClass.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 23/12/2022.
+//
+
+import Foundation
+import CoreData
+
+
+public class EpisodeManagedObject: NSManagedObject {
+
+}
+
