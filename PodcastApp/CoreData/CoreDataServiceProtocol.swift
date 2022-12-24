4e46541df6745246e0e8b20a099b2b87b2a676ae
Temi <ytemiloluwa@gmail.com>
2022-12-24 11:35:18 +0100

    [ Chore : Updated CoreDataserviceProtocol ]

--- PodcastApp/CoreData/CoreDataServiceProtocol.swift
+++ PodcastApp/CoreData/CoreDataServiceProtocol.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
-protocol CoreDataserviceProtocol {
+protocol CoreDataServiceProtocol {
     
     func saveGenres(_ genres: [GenreViewModel]) -> Bool
     func fetchGenres() -> [GenreViewModel]
     func saveEpisode(_ episode: EpisodeViewModel, audio: Data) -> Bool
     func fetchEpisode(_ episode: EpisodeViewModel) -> EpisodeManagedObject?
     func fetchPodcast(_ id: Int) -> PodcastManagedObject?
-    
+    func bookmarkPodcast(_ podcast: PodcastViewModel) -> PodcastManagedObject
+    func unbookmarkedPodcast(_ id: Int) -> PodcastManagedObject?
 }
