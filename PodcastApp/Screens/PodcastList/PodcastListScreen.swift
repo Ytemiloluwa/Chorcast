868000c12e27b06d92891dfb6fe8ee7ff1bf39a8
Temi <ytemiloluwa@gmail.com>
2022-12-25 13:02:06 +0100

    [ Chore : Updated PodcastListScreen ]

--- PodcastApp/Screens/PodcastList/PodcastListScreen.swift
+++ PodcastApp/Screens/PodcastList/PodcastListScreen.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct PodcastListScreen: View {
+    
     var genre: String
+    
+    @EnvironmentObject private var store: Store
     
     private let gridItems: [GridItem] = Array(repeating: .init(.adaptive(minimum: 350), spacing: 20), count: 1)
     
     private var grid: some View {
         
-        LazyVGrid(columns: gridItems, alignment: .leading, spacing: 10, content: { PodcastListContent(genre: genre, limit: 100)})
+        LazyVGrid(columns: gridItems, alignment: .leading, spacing: 10, content: {
+            
+            PodcastListContent(state: getState(for: genre), fetchPodcasts: fetchPodcasts)
+        })
         
     }
     var body: some View {
@@ -24,6 +30,22 @@ struct PodcastListScreen: View {
             grid
             
         }.navigationTitle(genre)
+    }
+    
+    private func fetchPodcasts() {
+        
+        store.dispatch(.api(.fetchPodcasts(genre, limit: 10)))
+    }
+    
+    private func getState(for term: String) -> AppState.Result<[PodcastViewModel]> {
+        
+        if let state = store.apiState.preferredPodcasts[term] {
+            
+            return state
+        }
+        
+        return .loading
+        
     }
 }
 
