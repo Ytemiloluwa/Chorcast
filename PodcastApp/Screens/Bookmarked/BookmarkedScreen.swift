1b9f4b7c931f1c2e369f5cdb49d22fa24779fc2c
Temi <ytemiloluwa@gmail.com>
2022-12-25 10:24:36 +0100

    [ Chore : Added BookmarkedScreen ]

---
+++ PodcastApp/Screens/Bookmarked/BookmarkedScreen.swift
@@ -0,0 +1,70 @@
+//
+//  BookmarkedScreen.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 25/12/2022.
+//
+
+import SwiftUI
+
+struct BookmarkedScreen: View {
+    
+    @EnvironmentObject private var store: Store
+    
+    private let gridItems: [GridItem] = Array(repeating: .init(.adaptive(minimum: 350), spacing: 20), count: 1)
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            switch store.coreDataState.bookmarkedPodcasts {
+                
+            case.loading:
+                RedactedListView(viewType: .podcast)
+            case.success(let podcast):
+                createGrid(podcasts: podcast)
+            case.failure:
+                FailureView(message: "Unable to fetch podcasts", retryAction: fetchPodcasts)
+            }
+        }.navigationTitle("Bookmark")
+    }
+    
+    private func fetchPodcasts() {
+        
+        store.dispatch(.coreData(.fetchBookmarkedPodcasts))
+    }
+    
+    private func createContent(podcasts: [PodcastViewModel]) -> some View {
+        
+        ForEach(podcasts) { podcast in
+            
+            PodcastLinkView(podcast: podcast)
+            
+        }.padding(.horizontal)
+    }
+    
+    private func createGrid(podcasts: [PodcastViewModel]) -> some View {
+        
+        LazyVGrid(columns: gridItems, alignment: .leading, spacing: 10, content: { createContent(podcasts: podcasts)})
+    }
+    
+}
+
+struct BookmarkedScreen_Previews: PreviewProvider {
+    
+    static let store = Store(enviroment: AppEnvironment(api: previewApiService(), coredata: PreviewCoreDataService()))
+    static var previews: some View {
+        
+        Group {
+            
+            BookmarkedScreen()
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+            
+            BookmarkedScreen()
+                .preferredColorScheme(.light)
+                .previewLayout(.sizeThatFits)
+            
+        }.environmentObject(store)
+    }
+}
