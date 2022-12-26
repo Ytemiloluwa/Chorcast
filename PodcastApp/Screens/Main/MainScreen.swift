cc6e1f72a5749a0c859e6a8f189cf7fb57fac355
Temi <ytemiloluwa@gmail.com>
2022-12-26 12:33:18 +0100

    [ Chore : Added MainScreen ]

---
+++ PodcastApp/Screens/Main/MainScreen.swift
@@ -0,0 +1,51 @@
+//
+//  MainScreen.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 26/12/2022.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    
+    #if os(iOS)
+    
+    @Environment(\.horizontalSizeClass) private var hSizeClass: UserInterfaceSizeClass?
+    
+    #endif
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            if hSizeClass == .regular {
+                
+                Text("Sizebar menus")
+                
+            } else {
+                
+                HomeScreen()
+            }
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    
+    static let store = Store(enviroment: AppEnvironment(api: previewApiService(), coredata: PreviewCoreDataService()))
+    
+    static var previews: some View {
+        Group {
+            
+            MainScreen()
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+            
+            MainScreen()
+                .preferredColorScheme(.light)
+                .previewLayout(.sizeThatFits)
+            
+        }.environmentObject(store)
+    }
+}
