ae3af667b420cf4f38b98702e104cd644e7a7a97
Temi <ytemiloluwa@gmail.com>
2022-12-26 20:14:33 +0100

    [ Chore : Updated PodcastDetailContent ]

--- PodcastApp/Screens/PodcastDetails/Components/PodcastDetailContent.swift
+++ PodcastApp/Screens/PodcastDetails/Components/PodcastDetailContent.swift
@@ -13,25 +13,9 @@ struct PodcastDetailContent: View {
     
     @EnvironmentObject private var store: Store
     
-    private var bookmarkButton: some View {
-        
-        Button(action: {
-            
-            if podcast.isBookmarked {
-                
-                store.dispatch(.coreData(.deleteBookmark(podcast)))
-            }else {
-                
-                store.dispatch(.coreData(.bookmark(podcast)))
-            }
-            
-        }, label: {
-            
-            Image(systemName: podcast.isBookmarked ? "bookmark.fill" : "bookmark")
-            
-        })
-
-    }
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) private var hSizeClass: UserInterfaceSizeClass?
+    #endif
     
     private var description: some View {
         
@@ -47,32 +31,95 @@ struct PodcastDetailContent: View {
             Label("See all \(podcast.episodes.count) episodes", systemImage: "square.stack.3d.down.right.fill")
         }.padding(.horizontal)
     }
-    var body: some View {
+    
+    private var bookmarkButton: some View {
         
-        GeometryReader { geometry  in
+        Button(action: {
+            
+            if podcast.isBookmarked {
+                
+                store.dispatch(.coreData(.unbookmarked(podcast)))
+            }else {
+                
+                store.dispatch(.coreData(.bookmark(podcast)))
+            }
+            
+        }, label: {
+            
+            Image(systemName: podcast.isBookmarked ? "bookmark.fill" : "bookmark")
+            
+        })
+
+    }
+    
+    var compactContent: some View {
+        
+        GeometryReader { geometry in
             
             ScrollView(showsIndicators: false, content: {
-            
-                VStack {
+                
+                VStack{
                     
                     createImage(size: geometry.size.width)
                     
                     VStack(alignment: .leading, spacing: 10) {
-                        
                         PodcastDetailHeaderComponent(podcast: podcast)
                         allEpiosdeLink
                         Divider()
                         description
                     }
                 }
-            }).navigationBarTitle("", displayMode: .inline)
-                .navigationBarItems(trailing: bookmarkButton)
+            })
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarItems(trailing: bookmarkButton)
         }
     }
     
-    private func createImage(size: CGFloat) -> some View {
+    var regularContent: some View {
         
-        SCImage(podcast: podcast, size: size, cornerRadius: 0)
+        HStack {
+            
+            GeometryReader { geometry in
+                
+                ScrollView(showsIndicators: false, content: {
+                    
+                    VStack {
+                        
+                        createImage(size: geometry.size.width, cornerRadius: 20)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            
+                            PodcastDetailHeaderComponent(podcast: podcast)
+                            description
+                        }
+                    }
+                })
+            }
+            EpisodeListScreen(episodes: podcast.episodes)
+            
+        }.navigationBarTitle("", displayMode: .inline)
+            .padding(.horizontal)
+            .toolbar {
+                
+                bookmarkButton
+            }
+    }
+    
+    var body: some View {
+        
+        if hSizeClass == .compact {
+            
+            compactContent
+            
+        }else {
+            
+            regularContent
+        }
+    }
+    
+    private func createImage(size: CGFloat, cornerRadius: CGFloat = 0) -> some View {
+        
+        SCImage(podcast: podcast, size: size, cornerRadius: cornerRadius)
             .aspectRatio(contentMode: .fill)
     }
 }
