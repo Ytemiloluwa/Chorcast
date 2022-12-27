98db8d349295442ce2f930511e485eae96919e69
Temi <ytemiloluwa@gmail.com>
2022-12-27 20:38:38 +0100

    [ Chore : Updated PodcastPlayer ]

--- PodcastApp/SharedUI/PodcastPlayer.swift
+++ PodcastApp/SharedUI/PodcastPlayer.swift
@@ -10,7 +10,7 @@ import AVFoundation
 import MediaPlayer
 
 class PodcastPlayer: NSObject {
-
+    
     var player = AVPlayer()
     
     //keeping track of object returned by various observers
@@ -24,9 +24,45 @@ class PodcastPlayer: NSObject {
     var onTogglePlay: ((Bool) -> Void) = { _ in }
     var onFailure: ((String) -> Void) = { _ in }
     
+    override init() {
+        
+        super.init()
+        
+        player = AVPlayer()
+    }
+    
     func play(url: URL) {
         
         let asset = AVAsset(url: url)
+        let playerItem = AVPlayerItem(asset: asset)
+        player.replaceCurrentItem(with: playerItem)
+        prepareAsset(asset)
     }
     
+    
+    
+    private func validatesValues(for keys: [String], asset: AVAsset) -> Bool {
+        
+        return true
+    }
+    
+    private func setUpObservers() {
+        
+        
+    }
+    
+    private func prepareAsset(_ asset: AVAsset) {
+        
+        let playableKeys = ["playable", "hasProtectedContent"]
+        
+        asset.loadValuesAsynchronously(forKeys: playableKeys) { [weak self] in
+            
+            guard let self = self else { return }
+            
+            if self.validatesValues(for: playableKeys, asset: asset) {
+                
+                self.setUpObservers()
+            }
+        }
+    }
 }
