3b0c18db46d28d6f2a46217e81f52553c1a9bd9d
Temi <ytemiloluwa@gmail.com>
2022-12-28 21:40:46 +0100

    [ Chore : Added Data extension ]

---
+++ PodcastApp/Extension/Data+.swift
@@ -0,0 +1,37 @@
+//
+//  .Data+swift.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 28/12/2022.
+//
+
+import Foundation
+
+extension Data {
+    
+    
+    func write(to filename: String) -> URL {
+        
+        
+        var dir = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        
+        let location = dir.appendingPathComponent(filename + ".mp3")
+        
+        
+        if FileManager.default.fileExists(atPath: location!.relativePath) {
+            
+            return location!
+        }
+        
+        do {
+            
+            try self.write(to: location!, options: WritingOptions.atomicWrite)
+            
+        }catch let error {
+            
+            Log.error(error)
+        }
+        
+        return location!
+    }
+}
