84aa336ce529f1ece0b6b50dda526bfa27a97378
Temi <ytemiloluwa@gmail.com>
2022-12-29 00:28:19 +0100

    [ Chore : Added PlayerButton ]

---
+++ PodcastApp/SharedUI/Player/PlayerButton.swift
@@ -0,0 +1,47 @@
+//
+//  PlayerButton.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 29/12/2022.
+//
+
+import SwiftUI
+
+struct PlayerButton: View {
+    var action = {}
+    var play = ""
+    var isBig = false
+    
+    var body: some View {
+        
+        let size: CGFloat = isBig ? 30 : 20
+        
+        return Button(action: action, label: {
+            
+            Image(systemName: play)
+                .resizable()
+                .font(.system(size: size, weight: .light, design: .rounded))
+                .scaledToFit()
+                .frame(width: size, height: size, alignment: .center)
+                .foregroundColor(Color.gray)
+            
+        })
+    }
+}
+
+struct PlayerButton_Previews: PreviewProvider {
+    
+    static var previews: some View {
+       
+        Group {
+            
+            PlayerButton()
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+          
+            PlayerButton()
+                .preferredColorScheme(.light)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
