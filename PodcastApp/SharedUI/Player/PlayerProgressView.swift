57bffdf9091acff5ff5260f8bdb7ae1aba2a55e4
Temi <ytemiloluwa@gmail.com>
2022-12-29 17:02:15 +0100

    [ Chore : Added PlayerProgressView ]

---
+++ PodcastApp/SharedUI/Player/PlayerProgressView.swift
@@ -0,0 +1,56 @@
+//
+//  PlayerProgressView.swift
+//  PodcastApp
+//
+//  Created by Temiloluwa on 29/12/2022.
+//
+
+import SwiftUI
+
+struct PlayerProgressView: View {
+    
+    var currentTime: Double
+    var totalTime: Double
+    var onFinishSeeking: ((Double) -> Void) = { _ in }
+    
+    var body: some View {
+        
+        
+        VStack {
+            
+            ScrollBar(currentTime: .constant(currentTime), totalTime: totalTime, finish: { value in
+                
+                
+                onFinishSeeking(totalTime * value)
+                
+            }).frame(height: 15, alignment: .center)
+            
+            HStack {
+                
+                Text("\(currentTime.asString(style: .positional))")
+                    .font(.caption)
+                Spacer()
+                
+                Text("\(totalTime.asString(style: .positional))")
+                    .font(.caption)
+            }
+        }.frame(height: 40, alignment: .center)
+    }
+}
+struct PlayerProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            
+            PlayerProgressView(currentTime: 2000, totalTime: 3000)
+
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+                
+            
+            PlayerProgressView(currentTime: 2000, totalTime: 3000)
+            
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.light)
+        }
+    }
+}
